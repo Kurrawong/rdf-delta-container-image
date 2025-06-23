ffbbb6cfae3a4bf74f3e12846267cbda57564a03
Edmond Chuc <edmond@kurrawong.ai>
2025-06-23 17:12:27 +1000

    feat: add JAVA_OPTS to entrypoint scripts

--- fuseki-entrypoint.sh
+++ fuseki-entrypoint.sh
@@ -2,6 +2,9 @@
 
 exec \
   java \
+  ${JAVA_OPTS} \
+  --enable-native-access=ALL-UNNAMED \
+  --add-modules jdk.incubator.vector \
   -cp "/opt/rdf-delta/rdf-delta-fuseki-server.jar:/opt/rdf-delta/compoundnaming.jar" \
   org.seaborne.delta.fuseki.cmd.DeltaFusekiServerCmd \
   --conf config.ttl
